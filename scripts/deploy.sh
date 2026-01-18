52c9f54befb9da0aa11004b8e59d1eb02c3b9ba5
Nikhilkumar2311 <nk4873300@gmail.com>
2026-01-18 18:14:34 +0530

    Added EC2

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+APP_NAME="ci-cd-pipeline"
+IMAGE="$1"
+
+echo "🚀 Deploying image: $IMAGE"
+
+docker pull $IMAGE
+
+if docker ps -a | grep -q $APP_NAME; then
+  echo "Stopping existing container..."
+  docker stop $APP_NAME
+  docker rm $APP_NAME
+fi
+
+docker run -d \
+  --name $APP_NAME
+  -p 3000:3000 \
+  $IMAGE
+
+echo "Deployment complete ✅"
